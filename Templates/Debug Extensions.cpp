b51bf3eb086c999de8c97c6eeedb1a2539e31299
bidhan <bidhan@ubuntu.ubuntu-domain>
2014-12-14 06:59:56 -0500

    Added Debug Template Extensions

---
+++ Templates/Debug Extensions.cpp
@@ -0,0 +1,43 @@
+/// ********* debug template *********
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+    return os << "{ " << p.first << ", " << p.second << " }";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+    os << " Vector [ ";
+    typename vector< T > :: const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << " ]";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+    os << " Set [ ";
+    typename set< T > :: const_iterator it;
+    for ( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << *it;
+    }
+    return os << " ]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+    os << " Map [ ";
+    typename map< F , S >::const_iterator it;
+    for( it = v.begin(); it != v.end(); it++ ) {
+        if( it != v.begin() ) os << ", ";
+        os << it -> first << " = " << it -> second ;
+    }
+    return os << " ]";
+}
+
+#define deb(x) cerr << #x << x << endl;
+
+/// ********* debug template *********
---
+++ Templates/online contest template (C++11).cpp
@@ -0,0 +1,41 @@
+/*
+ * Bidhan Roy
+ * University of Dhaka
+ */
+ 
+using namespace std;
+#include <bits/stdc++.h>
+ 
+#define rep(i,n) for(auto i=0; i<(n); i++)
+#define mem(x,val) memset((x),(val),sizeof(x));
+#define rite(x) freopen(x,"w",stdout);
+#define read(x) freopen(x,"r",stdin);
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define sqr(x) ((x)*(x))
+#define pb push_back
+#define clr clear()
+#define inf (1<<30)
+#define ins insert
+#define xx first
+#define yy second
+#define eps 1e-9
+ 
+typedef long long i64;
+typedef unsigned long long ui64;
+typedef string st;
+typedef vector<int> vi;
+typedef vector<st> vs;
+typedef map<int,int> mii;
+typedef map<st,int> msi;
+typedef set<int> si;
+typedef set<st> ss;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+ 
+#define mx 0
+ 
+int main() {
+    ios_base::sync_with_stdio(0);
+    return 0;
+}
