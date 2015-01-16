5c32ee3ded540914bb4c772c121306f68949b1fd
Bidhan Roy <mail2bidhan@gmail.com>
2015-01-16 18:58:23 +0600

    edited debug extensions

--- Templates/Debug Extensions.cpp
+++ Templates/Debug Extensions.cpp
@@ -2,42 +2,42 @@
 
 template < typename F, typename S >
 ostream& operator << ( ostream& os, const pair< F, S > & p ) {
-    return os << "{ " << p.first << ", " << p.second << " }";
+    return os << "{" << p.first << ", " << p.second << "}";
 }
 
 template < typename T >
 ostream &operator << ( ostream & os, const vector< T > &v ) {
-    os << " Vector [ ";
+    os << "[";
     typename vector< T > :: const_iterator it;
     for( it = v.begin(); it != v.end(); it++ ) {
         if( it != v.begin() ) os << ", ";
         os << *it;
     }
-    return os << " ]";
+    return os << "]";
 }
 
 template < typename T >
 ostream &operator << ( ostream & os, const set< T > &v ) {
-    os << " Set [ ";
+    os << "[";
     typename set< T > :: const_iterator it;
     for ( it = v.begin(); it != v.end(); it++ ) {
         if( it != v.begin() ) os << ", ";
         os << *it;
     }
-    return os << " ]";
+    return os << "]";
 }
 
 template < typename F, typename S >
 ostream &operator << ( ostream & os, const map< F, S > &v ) {
-    os << " Map [ ";
+    os << "[";
     typename map< F , S >::const_iterator it;
     for( it = v.begin(); it != v.end(); it++ ) {
         if( it != v.begin() ) os << ", ";
         os << it -> first << " = " << it -> second ;
     }
-    return os << " ]";
+    return os << "]";
 }
 
-#define deb(x) cerr << #x << x << endl;
+#define deb(x) cerr << #x << " = " << x << endl;
 
 /// ********* debug template *********
