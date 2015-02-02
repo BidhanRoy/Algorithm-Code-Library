8c32945130ffc375ecbb6e01423fa4d664d8024f
Bidhan Roy <mail2bidhan@gmail.com>
2015-02-02 13:49:51 +0600

    update

--- Data Structures/Minimum Subsequent Value.cpp
+++ Data Structures/Minimum Subsequent Value.cpp
@@ -1,16 +1,19 @@
 struct MyQ{
-	deque<int> D,Min;
-	void push(int val){
-        D.push_back(val);
-        while(!Min.empty() && Min.back()>val) Min.pop_back();
-        Min.push_back(val);
+    deque<int> D, Min;
+
+    void push( int val ) {
+        D.push_back( val );
+        while( !Min.empty() && Min.back() > val ) Min.pop_back();
+        Min.push_back( val );
     }
-    void pop(){
-         if(Min.front()==D.front() )
+
+    void pop() {
+         if( Min.front() == D.front() )
             Min.pop_front();
          D.pop_front();
     }
-    int get(){
+
+    int get() {
 		return Min.front();
 	}
 };
