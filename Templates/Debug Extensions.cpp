e85b18b79b83bb62100cb588fda8b0e9c2bcd638
Bidhan Roy <bidhanroy@MAC-10001364.local>
2015-07-26 14:02:47 +0600

    edit

--- Templates/Debug Extensions.cpp
+++ Templates/Debug Extensions.cpp
@@ -1,4 +1,4 @@
-/// ********* debug template *********
+/// ********* debug template bt Bidhan Roy *********
 
 template < typename F, typename S >
 ostream& operator << ( ostream& os, const pair< F, S > & p ) {
